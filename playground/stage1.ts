355d9fdd580223a3c54e721cecabd5f2de9028b1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-11-18 18:35:40 +0100

    Fix bundling now that stage2 has been moved

--- playground/stage1.ts
+++ playground/stage1.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run --allow-read=. --allow-env=DENO_DEPLOYMENT_ID --allow-net=0.0.0.0 --no-check -L debug
 
 // Mimics Netlify's stage 1 loader without using eszip.
-// See bundler/stage1.ts for the actual bundling code.
+// See src/bundler/stage1.ts for the actual bundling code.
 
 import { boot } from "https://edge.netlify.com/bootstrap/index-stage1.ts";
 
--- playground/stage2.ts
+++ playground/stage2.ts
@@ -1,11 +1,7 @@
 // Mimics Netlify's stage 2 loader without using eszip.
-// See bundler/stage2.ts for the actual bundling code.
-//
-// Edge bundler also reimplements a loader for local dev:
-// https://github.com/netlify/edge-bundler/blob/v1.1.0/src/formats/javascript.ts#L106
 //
 // Note that routing happens outside of the bootstrap code. See Netlify CLI:
-// https://github.com/netlify/cli/blob/v10.3.0/src/lib/edge-functions/proxy.js#L92
+// https://github.com/netlify/cli/blob/v12.2.3/src/lib/edge-functions/proxy.cjs#L112
 
 import { EdgeFunction } from "netlify:edge";
 

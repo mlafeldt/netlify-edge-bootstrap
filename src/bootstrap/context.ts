1856e715843d136c4b1b8c37b67e0744e0a1d4aa
mlafeldt <mlafeldt@users.noreply.github.com>
2023-03-23 00:04:01 +0000

    Sync bootstrap code

--- src/bootstrap/context.ts
+++ src/bootstrap/context.ts
@@ -12,18 +12,24 @@ interface Context {
    * @deprecated Use [`Response.json`](https://fetch.spec.whatwg.org/#ref-for-dom-response-json①) instead.
    */
   json(input: unknown, init?: ResponseInit): Response;
+
   /**
    * @deprecated Use `console.log` instead.
    */
   log(...data: unknown[]): void;
 
   next(options?: NextOptions): Promise<Response>;
+
   /**
    * @param request `Request` to be passed down the request chain. Defaults to the original `request` object passed into the Edge Function.
    */
   next(request: Request, options?: NextOptions): Promise<Response>;
 
   requestId: string;
+
+  /**
+   * @deprecated Use a `URL` object instead: https://ntl.fyi/edge-rewrite
+   */
   rewrite(url: string | URL): Promise<Response>;
   site: Site;
   account: Account;

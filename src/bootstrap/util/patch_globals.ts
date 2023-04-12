44af8b3fa4f0fc95e6d8d35b3608fbe0c9c5a2cd
mlafeldt <mlafeldt@users.noreply.github.com>
2023-04-12 00:03:53 +0000

    Sync bootstrap code

--- src/bootstrap/util/patch_globals.ts
+++ src/bootstrap/util/patch_globals.ts
@@ -1,3 +1,4 @@
+import { patchDenoFS } from "../deno-fs.ts";
 import { patchLogger } from "../log/instrumented_log.ts";
 import { Metadata } from "../stage_2.ts";
 import { patchResponseRedirect } from "../util/redirect.ts";
@@ -9,6 +10,17 @@ export const patchGlobals = (metadata?: Metadata) => {
   globalThis.console.debug = patchLogger(globalThis.console.debug, metadata);
   globalThis.console.warn = patchLogger(globalThis.console.warn, metadata);
   globalThis.console.info = patchLogger(globalThis.console.info, metadata);
+
+  // https://deno.com/deploy/docs/runtime-fs
+  globalThis.Deno.cwd = patchDenoFS(globalThis.Deno.cwd);
+  globalThis.Deno.readDir = patchDenoFS(globalThis.Deno.readDir);
+  globalThis.Deno.readFile = patchDenoFS(globalThis.Deno.readFile);
+  globalThis.Deno.readTextFile = patchDenoFS(globalThis.Deno.readTextFile);
+  globalThis.Deno.open = patchDenoFS(globalThis.Deno.open);
+  globalThis.Deno.stat = patchDenoFS(globalThis.Deno.stat);
+  globalThis.Deno.lstat = patchDenoFS(globalThis.Deno.lstat);
+  globalThis.Deno.realPath = patchDenoFS(globalThis.Deno.realPath);
+  globalThis.Deno.readLink = patchDenoFS(globalThis.Deno.readLink);
 
   Response.redirect = patchResponseRedirect(Response.redirect, metadata);
 };

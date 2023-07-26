b66d7a3594a548c639ae1922fa77b5aef75d3e04
mlafeldt <mlafeldt@users.noreply.github.com>
2023-07-26 00:04:17 +0000

    Sync bootstrap code

--- src/bootstrap/util/patch_globals.ts
+++ src/bootstrap/util/patch_globals.ts
@@ -3,6 +3,33 @@ import { patchLogger } from "../log/instrumented_log.ts";
 import { Metadata } from "../stage_2.ts";
 import { patchResponseRedirect } from "../util/redirect.ts";
 
+const LABEL_SEPARATOR = " ";
+
+export const patchTimeLogging = (
+  time: typeof console.time,
+  timeLog: typeof console.timeLog,
+  timeEnd: typeof console.timeEnd,
+  metadata?: Metadata,
+) => {
+  // console.time/timeLog/timeEnd use the first argument as a label.
+  // we prefix it with the Netlify metadata, so that the labels are scoped to the request & function.
+  // this only works if metadata is stable between two calls to `console.time`.
+  // we can't fully guarantee this, but it's the best solution we have for now.
+  globalThis.console.time = patchLogger(
+    (...args) => time(args.join(LABEL_SEPARATOR)),
+    metadata,
+  );
+  globalThis.console.timeLog = patchLogger(
+    (nfMeta, label, ...args) =>
+      timeLog([nfMeta, label].join(LABEL_SEPARATOR), ...args),
+    metadata,
+  );
+  globalThis.console.timeEnd = patchLogger(
+    (...args) => timeEnd(args.join(LABEL_SEPARATOR)),
+    metadata,
+  );
+};
+
 export const patchGlobals = (metadata?: Metadata) => {
   // https://developer.mozilla.org/en-US/docs/Web/API/console#instance_methods
   globalThis.console.log = patchLogger(globalThis.console.log, metadata);
@@ -10,6 +37,14 @@ export const patchGlobals = (metadata?: Metadata) => {
   globalThis.console.debug = patchLogger(globalThis.console.debug, metadata);
   globalThis.console.warn = patchLogger(globalThis.console.warn, metadata);
   globalThis.console.info = patchLogger(globalThis.console.info, metadata);
+  globalThis.console.trace = patchLogger(globalThis.console.trace, metadata);
+
+  patchTimeLogging(
+    (label) => globalThis.console.time(label),
+    (label, ...data) => globalThis.console.timeLog(label, ...data),
+    (label) => globalThis.console.timeEnd(label),
+    metadata,
+  );
 
   // https://deno.com/deploy/docs/runtime-fs
   globalThis.Deno.cwd = patchDenoFS(globalThis.Deno.cwd);

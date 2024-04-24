e33e8aa61a982906b22ff88c466c9ddfbe4e3337
mlafeldt <mlafeldt@users.noreply.github.com>
2024-04-24 00:04:02 +0000

    Sync bootstrap code

--- src/bootstrap/config.ts
+++ src/bootstrap/config.ts
@@ -6,15 +6,48 @@ type OnError = "fail" | "bypass" | Path;
 
 type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS";
 
+/**
+ * Netlify Edge Function inline configuration options.
+ *
+ * @see {@link https://docs.netlify.com/edge-functions/declarations/#declare-edge-functions-inline}
+ */
 export interface Config {
   cache?: Cache;
   excludedPath?: Path | Path[];
+  excludedPattern?: string | string[];
   onError?: OnError;
   path?: Path | Path[];
+  pattern?: string | string[];
   method?: HTTPMethod | HTTPMethod[];
 }
 
+/**
+ * Framework-generated Netlify Edge Function inline configuration options.
+ *
+ * @see {@link https://docs.netlify.com/edge-functions/create-integration/#generate-declarations}
+ */
 export interface IntegrationsConfig extends Config {
   name?: string;
   generator?: string;
+}
+
+/**
+ * A function configuration in the `manifest.json` file for framework-generated Netlify
+ * Edge Functions.
+ *
+ * @see {@link https://docs.netlify.com/edge-functions/declarations/#declare-edge-functions-inline}
+ */
+export interface ManifestFunction extends Config {
+  function: string;
+}
+
+/**
+ * The `manifest.json` file for framework-generated Netlify Edge Functions.
+ *
+ * @see {@link https://docs.netlify.com/edge-functions/create-integration/#generate-declarations}
+ */
+export interface Manifest {
+  version: 1;
+  functions: ManifestFunction[];
+  import_map?: string;
 }

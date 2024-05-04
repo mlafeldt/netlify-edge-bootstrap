6d26a14222edf943f14eb1c5c28d9b90af45fa5d
mlafeldt <mlafeldt@users.noreply.github.com>
2024-05-04 00:04:20 +0000

    Sync bootstrap code

--- src/bootstrap/log/logger.ts
+++ src/bootstrap/log/logger.ts
@@ -3,10 +3,14 @@ import {
   LogLevel,
   StructuredLogger,
 } from "https://v1-7-0--edge-utils.netlify.app/logger/mod.ts";
+import { instrumentedLog } from "./instrumented_log.ts";
 
 // An instance of `StructuredLogger` that uses an unpatched `console.log`. In
 // practice, this makes it detached from the context-tracking logic, and can
 // be used in situations where tracking the context is not necessary.
-const detachedLogger = logger.withRawLogger(globalThis.console.log);
+const unpatchedConsoleLog = globalThis.console.log;
+const detachedLogger = logger.withRawLogger((...data) =>
+  instrumentedLog(unpatchedConsoleLog, data)
+);
 
 export { detachedLogger, logger, LogLevel, StructuredLogger };
